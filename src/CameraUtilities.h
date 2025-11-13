c4306587b22e19ab2ee8ec8d8dda3f43484a1cc3
ihnryna <151572051+ihnryna@users.noreply.github.com>
2025-11-13 03:32:15 +0200

    Initial window and camera custom movement

---
+++ src/CameraUtilities.h
@@ -0,0 +1,24 @@
+//
+// Created by User on 13.11.2025.
+//
+#ifndef MESHCUTTER_CAMERAUTILITIES_H
+#define MESHCUTTER_CAMERAUTILITIES_H
+#define MY_CAMERA_MOVE_SPEED 5.4f
+#include "rcamera.h"
+
+inline void UpdateCameraPressAndDrag(Camera *camera, const int camera_mode) {
+    const float cameraMoveSpeed = MY_CAMERA_MOVE_SPEED*GetFrameTime();
+
+    if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE)) {
+        UpdateCamera(camera, camera_mode);
+    } else {
+        if (IsKeyDown(KEY_W)) CameraMoveForward(camera, cameraMoveSpeed, true);
+        if (IsKeyDown(KEY_A)) CameraMoveRight(camera, -cameraMoveSpeed, true);
+        if (IsKeyDown(KEY_S)) CameraMoveForward(camera, -cameraMoveSpeed, true);
+        if (IsKeyDown(KEY_D)) CameraMoveRight(camera, cameraMoveSpeed, true);
+    }
+    if (IsKeyDown(KEY_SPACE)) CameraMoveUp(camera, cameraMoveSpeed);
+    if (IsKeyDown(KEY_LEFT_CONTROL)) CameraMoveUp(camera, -cameraMoveSpeed);
+}
+
+#endif //MESHCUTTER_CAMERAUTILITIES_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include "raylib.h"
+#include "CameraUtilities.h"
 
 int main() {
+    constexpr int screenWidth = 1920;
+    constexpr int screenHeight = 1080;
+    InitWindow(screenWidth, screenHeight, "mesh cutting project");
 
-    std::cout << "Everything is alright, this is my mesh cutting project!" << std::endl;
-    return 0;
+    Camera camera = {Vector3(50, 50, 50), Vector3(0, 0, 0), Vector3(0, 1, 0), 45.f, CAMERA_PERSPECTIVE};
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        UpdateCameraPressAndDrag(&camera, CAMERA_FIRST_PERSON);
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        BeginMode3D(camera);
+        DrawGrid(20, 15.f);
+        EndMode3D();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
